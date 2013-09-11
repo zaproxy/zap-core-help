159e9157610a4315405c1b5e5a1327498a502982
psiinon <psiinon@gmail.com>
2013-09-11 09:11:59 +0000

    2.2 branch

---
+++ 2.2/src/help/zaphelp/ZapHelp.hs
@@ -0,0 +1,70 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
+         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+
+<helpset version="2.0">
+
+  <!-- title -->
+  <title>ZAP - User Guide</title>
+
+  <!-- maps -->
+  <maps>
+     <homeID>top</homeID>
+     <mapref location="Map.jhm"/>
+  </maps>
+
+  <!-- views -->
+  <view mergetype="javax.help.UniteAppendMerge">
+    <name>TOC</name>
+    <label>Contents</label>
+    <type>javax.help.TOCView</type>
+    <data>ZapHelpTOC.xml</data>
+  </view>
+
+  <view mergetype="javax.help.SortMerge">
+    <name>Index</name>
+    <label>Index</label>
+    <type>javax.help.IndexView</type>
+    <data>ZapHelpIndex.xml</data>
+  </view>
+
+  <view>
+    <name>Search</name>
+    <label>Search</label>
+    <type>javax.help.SearchView</type>
+    <data engine="com.sun.java.help.search.DefaultSearchEngine">
+      JavaHelpSearch
+    </data>
+  </view>
+
+  <view>
+    <name>Favorites</name>
+    <label>Favorites</label>
+    <type>javax.help.FavoritesView</type>
+  </view>
+
+  <presentation default="true" displayviewimages="false">
+     <name>main window</name>
+     <size width="1000" height="600" />
+     <location x="200" y="200" />
+     <title>ZAP - User Guide</title>
+     <image>toplevelicon</image>
+     <toolbar>
+	<helpaction>javax.help.BackAction</helpaction>
+	<helpaction>javax.help.ForwardAction</helpaction>
+	<helpaction>javax.help.SeparatorAction</helpaction>
+	<helpaction>javax.help.HomeAction</helpaction>
+	<helpaction>javax.help.ReloadAction</helpaction>
+	<helpaction>javax.help.SeparatorAction</helpaction>
+	<helpaction>javax.help.PrintAction</helpaction>
+	<helpaction>javax.help.PrintSetupAction</helpaction>
+     </toolbar>
+  </presentation>
+  <presentation>
+     <name>main</name>
+     <size width="400" height="400" />
+     <location x="200" y="200" />
+     <title>ZAP - User Guide</title>
+  </presentation>
+</helpset>
