daaa967caa6607e7278ba637dbcf596287e5253e
thc202 <thc202@gmail.com>
2019-05-21 09:00:56 +0100

    Update Gradle and add-on plugin
    Update Gradle to 5.4.1 and add-on plugin to 0.2.0.
    Update build accordingly.
    Add GitHub Actions workflow and Gradle task to release on tag.
    
    Signed-off-by: thc202 <thc202@gmail.com>

--- commonFiles/commonFiles.gradle.kts
+++ commonFiles/commonFiles.gradle.kts
@@ -3,3 +3,8 @@ plugins {
 }
 
 description = "Common files (e.g. images, map.jhm) used by all add-ons."
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
