82e61ab1fd84deecb6d8c99c22e4eaefd43f9887
zapbot <12745184+zapbot@users.noreply.github.com>
2025-01-13 06:41:08 +0000

    Update localized resources
    Update resources from Crowdin.
    
    Signed-off-by: zapbot <12745184+zapbot@users.noreply.github.com>

--- addOns/help_pl_PL/src/main/javahelp/helpset_pl_PL.hs
+++ addOns/help_pl_PL/src/main/javahelp/helpset_pl_PL.hs
@@ -2,7 +2,7 @@
 <helpset version="2.0" xml:lang="pl-PL">
 
   <!-- title -->
-  <title>ZAP - Desktop User Guide</title>
+  <title>ZAP by Checkmarx - Desktop User Guide</title>
 
   <!-- maps -->
   <maps>
